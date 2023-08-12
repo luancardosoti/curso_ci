dce7e92f48ff123341e89350d41488ac2ad3652b
Luan Nunes Cardoso <luan.cardoso@grendene.com.brls>
2023-08-11 21:21:12 -0300

    feat: test print

--- database/db.go
+++ database/db.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"github.com/guilhermeonrails/api-go-gin/models"
@@ -14,6 +15,8 @@ var (
 )
 
 func ConectaComBancoDeDados() {
+	fmt.Println("test:", os.Getenv("DB_HOST"))
+	
 	stringDeConexao := "host="+os.Getenv("DB_HOST") +" user="+os.Getenv("DB_USER") +" password="+os.Getenv("DB_PASSWORD") +" dbname="+os.Getenv("DB_NAME") +" port="+os.Getenv("DB_PORT") +" sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
